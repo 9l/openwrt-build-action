7216caac676b8f5d2c3ffe506c81593628c9bac0
QING LIU <9l@users.noreply.github.com>
2020-01-21 12:24:39 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 #1. Modify default IP
-sed -i 's/192.168.1.1/10.0.0.99/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168.$((addr_offset++)).1/10.0.$((addr_offset++)).99/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.95/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/10.0.$((addr_offset++)).95/g' openwrt/package/base-files/files/bin/config_generate
