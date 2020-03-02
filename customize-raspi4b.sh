38a78b7e352b486da0a75f30fec1d84df1f001dd
QING LIU <9l@users.noreply.github.com>
2020-03-02 20:37:08 +0800

    Update customize-raspi4b.sh

--- customize-raspi4b.sh
+++ customize-raspi4b.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.91/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/192.168.$((addr_offset++)).1/192.168.$((addr_offset++)).91/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.91/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/10.0.$((addr_offset++)).91/g' openwrt/package/base-files/files/bin/config_generate
