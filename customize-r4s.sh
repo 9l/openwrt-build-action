158103f78bc7835f93423dcbc19e4ab125257f4d
9l <9l@users.noreply.github.com>
2021-01-26 16:58:09 +0800

    add nanopi r4s config files

---
+++ customize-r4s.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+#1. Modify default IP
+sed -i 's/192.168.1.1/10.8.3.245/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/10.8.$((addr_offset++)).245/g' openwrt/package/base-files/files/bin/config_generate
